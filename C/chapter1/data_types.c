5650ba726b9c21414cdca4764616c6fb564bc4dd
Saadat-B <saadatbadgujar@gmail.com>
2024-12-05 12:05:23 +0530

    data types

---
+++ C/chapter1/data_types.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a = 1;
+
+    float b;
+    b = 1.4;
+
+    // characters should always be in single quotes, double quotes is not accepted
+    char c = 'a';
+
+    return 0;
+}
