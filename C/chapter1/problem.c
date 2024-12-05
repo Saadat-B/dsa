2666fe315fbf21265b4b55ec5d3289cef0bef37c
Saadat-B <saadatbadgujar@gmail.com>
2024-12-05 14:35:44 +0530

    problems

---
+++ C/chapter1/problem.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int length;
+    int breadth;
+    scanf("%d", &length);
+    scanf("%d", &breadth);
+
+    printf("this is the area of the rectangle %d", length * breadth);
+
+    return 0;
+}
